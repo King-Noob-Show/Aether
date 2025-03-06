399cf453cefad419ad3e36400d214f3235d04177
King <kingnoobshow@gmail.com>
2025-03-06 11:38:33 +0530

    Created Main File And Added Error Handling

---
+++ src/errors/errorHandler.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+const c = require("ansi-colors");
+
+function errorHandler() {
+  const logFilePath = path.join(__dirname, "errors.txt");
+
+  console.log(c.redBright.bold("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+  console.log(c.redBright.bold("[ERROR HANDLER] Starting Error Handlers..."));
+
+  // Function to log errors to the file
+  function logErrorToFile(type, error) {
+    const errorMessage =
+      error?.stack ||
+      error?.message ||
+      JSON.stringify(error, null, 2) ||
+      "[ERROR] Unknown Error";
+    const logEntry = `[${new Date().toISOString()}] [${type}] ${errorMessage}\n\n`;
+
+    fs.appendFileSync("errors.txt", logEntry);
+  }
+
+  // Error Handlers
+  process.on("unhandledRejection", (reason, p) => {
+    console.error(c.red.bold(" [ERROR] Unhandled Rejection/Catch"));
+    // console.error(reason?.stack || reason); // Ensure stack trace is logged if available
+    logErrorToFile("Unhandled Rejection", reason);
+  });
+
+  process.on("uncaughtException", (err, origin) => {
+    console.error(c.red.bold(" [ERROR] Uncaught Exception/Catch"));
+    // console.error(err?.stack || err); // Log stack trace for debugging
+    logErrorToFile("Uncaught Exception", err);
+  });
+
+  process.on("uncaughtExceptionMonitor", (err, origin) => {
+    console.log(c.red.bold(" [ERROR] Uncaught Exception/Catch (MONITOR)"));
+    // console.log(c.red.bold(err), origin);
+    logErrorToFile("Uncaught Exception Monitor", err);
+  });
+
+  console.log(c.redBright.bold("[ERROR HANDLER] Started Error Handlers"));
+  console.log(c.redBright.bold("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+}
+
+module.exports = errorHandler;
---
+++ src/index.js
@@ -0,0 +1,94 @@
+// Imports
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const c = require("ansi-colors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const errorHandler = require("./errors/errorHandler");
+dotenv.config();
+
+// Creating A New Client
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ],
+  allowedMentions: {
+    parse: ["everyone", "roles", "users"],
+    repliedUser: true,
+    users: true,
+    roles: true,
+  },
+});
+
+// Exporting Client
+module.exports = client;
+
+// Connect To MongoDB
+async function initiateMongoDB() {
+  console.log(c.magenta.bold("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+  console.log(c.magenta.bold("[DATABASE] Connecting To MongoDB..."));
+
+  const mongo = process.env.MONGOOSE;
+  if (!mongo) {
+    console.log(c.red.bold("[ERROR] No MongoDB URI Provided."));
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(mongo);
+    console.log(c.magenta.bold("[DATABASE] Connected To MongoDB!"));
+    console.log(c.magenta.bold("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+  } catch (e) {
+    console.log(
+      c.red.bold(
+        "[ERROR] An Error Has Occurred During Connection To MongoDB.\n",
+        e.message,
+      ),
+    );
+    process.exit(1);
+  }
+}
+
+async function clientLogin() {
+  try {
+    // Token
+    const token = process.env.TOKEN;
+    if (!token) {
+      console.log(c.red.bold("[ERROR] No Token Provided."));
+      process.exit(1);
+    }
+    console.log(c.blue.bold("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+    console.log(c.blue.bold("[SERVER] Logging In..."));
+
+    // Logging In
+    await client.login(token);
+  } catch (error) {
+    console.error(c.red.bold("[ERROR] Failed to log in:"), error);
+    process.exit(1);
+  }
+}
+
+async function loadHandlers() {
+  // Dynamically Loading Handlers
+  console.log(c.yellow.bold("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+  console.log(c.yellow.bold("[HANDLERS] Loading Handlers..."));
+  const handlers = ["eventHandler", "messageHandler", "commandHandler"];
+  for (const handler of handlers) {
+    await require(`./handlers/${handler}`)(client);
+  }
+}
+
+// Loading Everything In.
+(async () => {
+  await errorHandler();
+  await initiateMongoDB();
+  await loadHandlers().then(() => {
+    console.log(c.yellow.bold("[HANDLERS] Loaded Handlers."));
+    console.log(c.yellow.bold("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+  });
+  await clientLogin().then(() => {
+    console.log(c.blue.bold("[SERVER] Logged In."));
+    console.log(c.blue.bold("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+  });
+})();
